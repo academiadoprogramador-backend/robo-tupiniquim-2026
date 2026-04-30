75215a80a98bbe7b71bcc933af46afcaceedf633
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-04-30 19:43:32 -0300

    Adicionada lógica do processamento de movimento

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -7,6 +7,22 @@
     D = N
 
     Comando = EMEMEMEMM
+
+    -----------------------------
+
+    Teste do algoritmo 2:
+
+    Pos. Inicial:
+    X = 3
+    Y = 3
+    D = L
+
+    Comando = MMDMMDMDDM
+
+    Pos. Final Esperada:
+    X = 5
+    Y = 1
+    D = L
 */
 
 int posicaoX = 1;
@@ -15,11 +31,35 @@ char orientacao = 'N';
 
 string comandoCompleto = "EMEMEMEMM";
 
-Console.WriteLine($"Posição inicial: X={posicaoX} Y={posicaoY} O={orientacao}");
+Console.WriteLine($"Posição inicial: {posicaoX} {posicaoY} {orientacao}");
 
 char[] instrucoes = comandoCompleto.ToCharArray();
 
 for (int contador = 0; contador < instrucoes.Length; contador++)
 {
     char instrucao = instrucoes[contador];
+
+    if (instrucao == 'E')
+    {
+        if (orientacao == 'N') orientacao = 'O';
+        else if (orientacao == 'O') orientacao = 'S';
+        else if (orientacao == 'S') orientacao = 'L';
+        else if (orientacao == 'L') orientacao = 'N';
+    }
+    else if (instrucao == 'D')
+    {
+        if (orientacao == 'N') orientacao = 'L';
+        else if (orientacao == 'L') orientacao = 'S';
+        else if (orientacao == 'S') orientacao = 'O';
+        else if (orientacao == 'O') orientacao = 'N';
+    }
+    else if (instrucao == 'M')
+    {
+        if (orientacao == 'N') posicaoY++;
+        else if (orientacao == 'S') posicaoY--;
+        else if (orientacao == 'O') posicaoX--;
+        else if (orientacao == 'L') posicaoX++;
+    }
 }
+
+Console.WriteLine($"Posição final: {posicaoX} {posicaoY} {orientacao}");
