2cbc079b04c4c104f89692781a5434e6688b0d80
Tiago Santini <tiagosantiniathayde@outlook.com>
2026-04-30 19:25:25 -0300

    Adicionado input de comandos

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+    Teste do algoritmo 1:
+
+    Pos. Inicial:
+    X = 1
+    Y = 2
+    D = N
+
+    Comando = EMEMEMEMM
+*/
+
+int posicaoX = 1;
+int posicaoY = 2;
+char orientacao = 'N';
+
+string comandoCompleto = "EMEMEMEMM";
+
+Console.WriteLine($"Posição inicial: X={posicaoX} Y={posicaoY} O={orientacao}");
+
+char[] instrucoes = comandoCompleto.ToCharArray();
+
+for (int contador = 0; contador < instrucoes.Length; contador++)
+{
+    char instrucao = instrucoes[contador];
+}
